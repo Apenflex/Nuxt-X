de46cf1ddc729cd38167a93d7008ac9e156b88e8
Svyatoslav <blackdbf@gmail.com>
2024-05-09 22:59:28 +0300

    refactor: Remove AuthBlock and AuthForm components
    The AuthBlock and AuthForm components were deleted as they are no longer needed. These components were refactored to improve responsiveness and layout. The changes also simplify the structure and remove redundant code in index.vue.
    
    Refactor SidebarLeft.vue to improve responsiveness and layout
    Update app.vue and components.scss by removing unnecessary lines

--- components/Authentication/LoginForm.vue
+++ components/Authentication/LoginForm.vue
@@ -44,14 +44,12 @@ const onSubmit = handleSubmit(() => {
                 type="password"
                 :error="passwordErrorMessage"
             />
-            <div>
-                <button
-                    type="submit"
-                    class="w-full flex justify-center py-2 px-4 border border-transparent rounded-full shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 defaultTransition"
-                >
-                    Login
-                </button>
-            </div>
+            <button
+                type="submit"
+                class="w-full flex justify-center py-2 px-4 border border-transparent rounded-full shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 defaultTransition"
+            >
+                Login
+            </button>
         </form>
     </div>
 </template>
